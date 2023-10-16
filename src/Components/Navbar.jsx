f424b6038fabaa514d3e70bcd760452ff368381d
Zaher Zeinni <zaherzeinni@hotmail.com>
2023-10-17 00:34:58 +0300

    to github

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,149 +1,33 @@
-import { Fragment, useState } from 'react'
-import ManualClose from "./ManualClose"
-import { Box } from '@chakra-ui/react'
-import { Dialog, Popover, Transition } from '@headlessui/react'
-import {
-  Bars3Icon,
-  XMarkIcon,
-} from '@heroicons/react/24/outline'
+import React from 'react';
+import { Box ,Link} from '@chakra-ui/react';
+import {ShoppingCartOutlined,LoginOutlined} from '@ant-design/icons'
 
-
-
-
-import { GlobalContext } from '../Context'
-import { useContext } from 'react'
-
-
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
-
-export default function Example() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
-  
+const Navbar = ({cart}) => {
   return (
-    <header className="bg-white">
-      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
-        <div className="flex lg:flex-1 ">
-          <a href="#" className="-m-1.5 p-1.5">
-            <img className="h-8 w-auto" src="https://ui8-fleet-html.herokuapp.com/img/logo-dark.svg" alt="" />
-          </a>
-        </div>
-        <div className="flex lg:hidden">
-          <button
-            type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-            onClick={() => setMobileMenuOpen(true)}
-          >
-            <span className="sr-only">Open main menu</span>
-            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-          </button>
-        </div>
-        <Popover.Group className="hidden lg:flex lg:gap-x-12">
-          <Popover className="relative">
-            <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
-        
-              
-            </Popover.Button>
+    
+      
+      <Box className='flex justify-between items-center p-4 z-[100] w-full bg-[#081621] h-[10vh]'>
+      <Link href="/">
+      <Box color={'white'} ml={5}>
+     <img src="home.png" alt="img" width={40} height={40} />
+      </Box>
+      </Link>
+      <Box alignItems={'flex-end'} maxW={'auto'} justifyItems={"flex-end"}>
+      <Box color={'white'} className='justify-between space-x-8'>
+         
+      <span className="absolute top-2 right-40 bg-red-600 rounded-2xl p-0.5">
+            {cart.length}
+            </span>
+         <ShoppingCartOutlined />Cart  
+         
+         <LoginOutlined /> Sign In
+      </Box>
+      </Box>
+      </Box>
+  
 
-            <Transition
-              as={Fragment}
-              enter="transition ease-out duration-200"
-              enterFrom="opacity-0 translate-y-1"
-              enterTo="opacity-100 translate-y-0"
-              leave="transition ease-in duration-150"
-              leaveFrom="opacity-100 translate-y-0"
-              leaveTo="opacity-0 translate-y-1"
-            >
-              <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
-                <div className="p-4">
-                  
-                </div>
-                </Popover.Panel>
-            </Transition>
-          </Popover>
-
-          <a href="#" className=" font-semibold leading-6 text-gray-400 text-xl hover:text-black">
-            Discovery
-          </a>
-          <a href="#" className=" font-semibold leading-6 text-gray-400 text-xl hover:text-black">
-            About
-          </a>
-          <a href="#" className=" font-semibold leading-6 text-gray-400 text-xl hover:text-black">
-            Contact Us
-          </a>
-        </Popover.Group>
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-        <Box mt="5px" >
-        <ManualClose />
-        </Box>
-        
-          <Box w="20px">
-
-          </Box>
-          <a href="#" className="font-semibold  bg-blue-500 w-39 leading-6 text-white text-xl border border-l-neutral-950 p-3 
-           rounded-full hover:bg-blue-800 duration-700 ">
-            Sign Up <span aria-hidden="true"></span>
-          </a>
-        </div>
-      </nav>
-      <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-        <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-            
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt=""
-              />
-            </a>
-            <button
-              type="button"
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
-              onClick={() => setMobileMenuOpen(false)}
-            >
-              <span className="sr-only">Close menu</span>
-              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-            </button>
-          </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-               
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Discovery
-                </a>
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  About
-                </a>
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Contact Us
-                </a>
-              </div>
-              <div className="py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Log in
-                </a>
-              </div>
-            </div>
-          </div>
-        </Dialog.Panel>
-      </Dialog>
-    </header>
-  )
+    
+  );
 }
+
+export default Navbar;
