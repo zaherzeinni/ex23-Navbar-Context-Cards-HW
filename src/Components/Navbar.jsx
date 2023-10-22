542c8b491ccd01efa5d072960e26a7a47b230122
Zaher Zeinni <zaherzeinni@hotmail.com>
2023-10-23 01:58:50 +0300

    to github

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,7 @@ import { React } from 'react';
 import { Box } from '@chakra-ui/react';
 import {ShoppingCartOutlined,LoginOutlined} from '@ant-design/icons'
 import { toast } from 'react-toastify';
-
+//
 
 
 const Navbar = ({cart,navigateTo,signuserOut,Link,isAuth,user,page}) => {
